772e493072672454c78f3c810cda0ae32dc09f20
jet-delossantos <59231126+jet-delossantos@users.noreply.github.com>
2021-08-25 12:39:43 +0800

    Added database function for satdata table

--- classes/satdata.class.php
+++ classes/satdata.class.php
@@ -141,8 +141,69 @@ class SatData extends Dbh {
         $sql = "INSERT INTO satdatameta (fileNameSatDataMeta, fileSatDataMeta, formatSatDataMeta, dateUploadedSatDataMeta, uploaderSatDataMeta) 
                 VALUES (?, ?, ?, ?, ?);";
         $stmt = $this->connect()->prepare($sql);
-
         if ($stmt->execute([$fileNameSatDataMeta, $fileSatDataMeta, $fileFormat, $dateUploadedSatDataMeta, $uploaderSatDataMeta])){
+            $sql = "SELECT idSatDataMeta FROM satdatameta ORDER BY idSatDataMeta DESC LIMIT 1";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            foreach($results as $row){
+                $last_id = $row['idSatDataMeta'];
+            }
+            if ($fileFormat == '16'){
+                $lines = explode("<br />", $fileSatDataMeta);
+                foreach ($lines as $line){
+                    $words = explode("\t", $line); 
+    
+                    $gst = $words[0]."\t".$words[1]."\t";
+                    $gst = str_replace("\r\n","",$gst);
+    
+                    $dataType = $words[2]."\t";
+    
+                    $time = "";
+                    foreach (range(3,6) as $index){
+                        $time = $time.$words[$index]."\t";  
+                    }
+    
+                    $sensordata = "";
+                    foreach (range(7,14) as $index){
+                        $sensordata = $sensordata.$words[$index]."\t";  
+                    }
+    
+                    $checksum = $words[15]."\r\n";
+    
+                    $sql1 = "INSERT INTO satdata (gstidSatData, datatypeSatData, timeSatData, sensorSatData, checksumSatData,idSatMetaData) 
+                    VALUES (?, ?, ?, ?, ?, ?);";
+                    $stmt1 = $this->connect()->prepare($sql1);
+                    $stmt1->execute([$gst, $dataType, $time, $sensordata, $checksum, $last_id]);
+                }
+            } else if ($fileFormat == '32'){
+                $lines = explode("<br />", $fileSatDataMeta);
+                foreach ($lines as $line){
+                    $words = explode("\t", $line); 
+    
+                    $gst = $words[0]."\t".$words[1]."\t";
+                    $gst = str_replace("\r\n","",$gst);
+    
+                    $dataType = $words[2]."\t";
+    
+                    $time = "";
+                    foreach (range(3,6) as $index){
+                        $time = $time.$words[$index]."\t";  
+                    }
+    
+                    $sensordata = "";
+                    foreach (range(7,30) as $index){
+                        $sensordata = $sensordata.$words[$index]."\t";  
+                    }
+    
+                    $checksum = "00"."\r\n";
+    
+                    $sql1 = "INSERT INTO satdata (gstidSatData, datatypeSatData, timeSatData, sensorSatData, checksumSatData, idSatMetaData) 
+                    VALUES (?, ?, ?, ?, ?, ?);";
+                    $stmt1 = $this->connect()->prepare($sql1);
+                    $stmt1->execute([$gst, $dataType, $time, $sensordata, $checksum, $last_id]);
+                }
+            } 
             header('Location:../dashboard.php?status=fileuploadsuccess');
         } else {
             header('Location:../dashboard.php?status=fileuploadfailed');
@@ -153,10 +214,11 @@ class SatData extends Dbh {
             
             //Delete row based on id      
     protected function deleteFile($id, $filelocation){
-        $sql = "DELETE FROM satdatameta WHERE idSatDataMeta = ?;";
+        $sql = "DELETE FROM satdatameta WHERE idSatDataMeta = ?;
+                DELETE FROM satdata WHERE idSatMetaData = ?";
         $stmt = $this->connect()->prepare($sql);
         
-        if ($stmt->execute([$id])) {
+        if ($stmt->execute([$id,$id])) {
             unlink($filelocation);
             header('Location:../dashboard.php?status=filedeletesuccess');
             exit();
