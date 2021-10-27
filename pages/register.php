ccdfd716ccf059a55c05f1077c0bf50772b745b1
nikkirain21 <nikkiraintolentino@gmail.com>
2021-10-28 00:27:37 +0800

    changed ui colors, check first

--- pages/register.php
+++ pages/register.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KyuTech Sat Data</title>  
-    <link rel="stylesheet" href="../assets/css/stylesheet.css">
+    
     <!--Font awesome and bootstrap links-->
     <script src="https://kit.fontawesome.com/fde27cd9e3.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -18,15 +18,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet" href="../assets/css/stylesheet.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
+
     </script>
-    <style>
-    body {
-      font-family: "Montserrat"
-    }
-    </style>
 </head>
 
     <?php
